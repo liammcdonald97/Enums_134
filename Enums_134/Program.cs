1121e9ef030b0c9ab15cdf1b43b3675efba7f3be
liammcdonald97 <liammcdonald97@gmail.com>
2018-11-09 20:46:36 -0500

    Add project files.

---
+++ Enums_134/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums_134
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the current day of the week: ");
+            string value = Console.ReadLine();
+            DaysOfTheWeek day;
+            try
+            {
+                day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), value);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                Console.WriteLine(ex.Message);               
+            }
+            Console.ReadLine();
+        }
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
